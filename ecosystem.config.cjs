62d031b77e391e8cc9508e319f77830e21375ed5
matteopolak <matteopolak@hotmail.com>
2023-09-10 16:16:07 -0400

    feat: update config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,17 @@
+require('dotenv/config');
+
 module.exports = {
 	apps : [
 		{
 			name: 'healthhunt',
 			script: './.output/server/index.mjs',
 			env: {
-				'PORT': 3020,
-				'NODE_ENV': 'production'
+				PORT: 3020,
+				NODE_ENV: 'production',
+				SECRET: process.env.SECRET,
+				DATABASE_URL: process.env.DATABASE_URL,
+				OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+				ORIGIN: process.env.ORIGIN,
 			}
 		}
 	]
